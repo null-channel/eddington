9c1472799ec7d5f63d9d1ddec56757ae9a2a0f03
s1ntaxe770r <cyberdev01@protonmail.com>
2023-06-13 00:38:11 +0100

    minimum code required to build

--- application/container-builder/internal/containers/dockerfile/containerd.go
+++ application/container-builder/internal/containers/dockerfile/containerd.go
@@ -2,6 +2,9 @@ package dockerfile
 
 import (
 	"context"
+	"io"
+	"os"
+	"path/filepath"
 
 	"github.com/moby/buildkit/client"
 	"github.com/sirupsen/logrus"
@@ -30,5 +33,37 @@ func NewBuilder(ctx context.Context) (*Builder, error) {
 }
 
 func (b *Builder) Build(opts BuildOpt) error {
+	ctx := context.Background()
+	solvOpts := b.createSolveOpt(opts.ImageName, ".", opts.Dockerfile)
 
+	// prob print out what's going on here
+	status := make(chan *client.SolveStatus)
+
+	b.client.Solve(ctx, nil, solvOpts, status)
+	return nil
+
+}
+
+func (b *Builder) createSolveOpt(imageName string, buildContext string, dockerfile string) client.SolveOpt {
+	return client.SolveOpt{
+		Exports: []client.ExportEntry{
+			{
+				Type: "image",
+				Attrs: map[string]string{
+					"name": imageName,
+				},
+				Output: func(m map[string]string) (io.WriteCloser, error) {
+					return os.Stdout, nil
+				},
+			},
+		},
+		LocalDirs: map[string]string{
+			"context":    buildContext,
+			"dockerfile": filepath.Dir(dockerfile),
+		},
+		Frontend: "dockerfile.v0",
+		FrontendAttrs: map[string]string{
+			"filename": filepath.Base(dockerfile),
+		},
+	}
 }
