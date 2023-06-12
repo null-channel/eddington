c18d3e5a6ae513831f88245704783de8167d7529
s1ntaxe770r <cyberdev01@protonmail.com>
2023-06-13 00:03:54 +0100

    some builder impl

--- application/container-builder/internal/containers/dockerfile/containerd.go
+++ application/container-builder/internal/containers/dockerfile/containerd.go
@@ -1,6 +1,34 @@
 package dockerfile
 
-func BuildDockerfile() error {
+import (
+	"context"
 
-	return nil
+	"github.com/moby/buildkit/client"
+	"github.com/sirupsen/logrus"
+)
+
+type BuildOpt struct {
+	Dockerfile string
+	ImageName  string
+	Tag        string
+}
+
+type Builder struct {
+	client *client.Client
+}
+
+func NewBuilder(ctx context.Context) (*Builder, error) {
+	client, err := client.New(ctx, "", client.WithFailFast())
+	if err != nil {
+		logrus.Panic("unable to create buildkit client error: ", err.Error())
+		return nil, err
+	}
+	return &Builder{
+		client: client,
+	}, nil
+
+}
+
+func (b *Builder) Build(opts BuildOpt) error {
+
 }
