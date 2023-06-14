a2661587a1fcd8b207db018e023de3e9041f32e7
s1ntaxe770r <cyberdev01@protonmail.com>
2023-06-14 13:57:13 +0100

    test dockerfile build

--- application/container-builder/main.go
+++ application/container-builder/main.go
@@ -1,42 +1,57 @@
 package main
 
+import (
+	"context"
+
+	"github.com/null-channel/eddington/application/container-builder/internal/containers/dockerfile"
+	"github.com/sirupsen/logrus"
+)
+
 // go-git is a highly extensible git implementation in pure Go.
 // https://github.com/go-git/go-git
 
-import (
-	"fmt"
-
-	"github.com/go-git/go-git/v5"
-)
-
 // Basic example of how to clone a repository using clone options.
 func main() {
-	url := "https://github.com/null-channel/eddington.git"
-	directory := "./tmp/eddington"
+	// url := "https://github.com/null-channel/eddington.git"
+	// directory := "./tmp/eddington"
 
-	r, err := git.PlainClone(directory, false, &git.CloneOptions{
-		URL:               url,
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
-	})
+	// r, err := git.PlainClone(directory, false, &git.CloneOptions{
+	// 	URL:               url,
+	// 	RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+	// })
 
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// // ... retrieving the branch being pointed by HEAD
+	// ref, err := r.Head()
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(ref)
+
+	// // ... retrieving the commit object
+	// commit, err := r.CommitObject(ref.Hash())
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println(commit)
+	ctx := context.Background()
+	b, err := dockerfile.NewBuilder(ctx)
 	if err != nil {
-		panic(err)
+		logrus.Panic("unable to create builder error: ", err.Error())
 	}
-	// ... retrieving the branch being pointed by HEAD
-	ref, err := r.Head()
-
+	opts := dockerfile.BuildOpt{
+		Dockerfile: "./Dockerfile",
+		ImageName:  "eddington",
+	}
+	err = b.Build(opts)
 	if err != nil {
-		panic(err)
+		logrus.Panic("unable to build image error: ", err.Error())
 	}
 
-	fmt.Println(ref)
-
-	// ... retrieving the commit object
-	commit, err := r.CommitObject(ref.Hash())
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(commit)
 }
