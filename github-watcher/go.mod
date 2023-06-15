7403681f599b902efe21a70089c2a009d8388c67
dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2023-06-14 23:24:46 -0400

    initial github watcher impl

---
+++ github-watcher/go.mod
@@ -0,0 +1,8 @@
+module github.com/null-channel/eddington/github-watcher
+
+go 1.20
+
+require (
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+)
---
+++ github-watcher/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/google/go-github/github"
+)
+
+func pollCommits(owner string, repo string, branch string, lastCommitSHA string, client *github.Client) {
+
+	ctx := context.Background()
+	// client := github.NewClient(nil)
+
+	opt := &github.CommitsListOptions{
+		SHA: branch,
+	}
+
+	commits, _, err := client.Repositories.ListCommits(ctx, owner, repo, opt)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	latestCommitSHA := *commits[0].SHA
+	if latestCommitSHA == lastCommitSHA {
+		fmt.Println("No updates to this branch")
+	} else {
+		fmt.Println("This branch has been updated")
+	}
+
+}
+
+func main() {
+	// ctx := context.Background()
+	client := github.NewClient(nil)
+
+	owner := "null-channel"
+	repo := "eddington"
+	branch := "main"
+	lastCommitSHA := "02514afee6fc9ada5225a4f9670ebc4e627d6e24"
+
+	// opt := &github.CommitsListOptions{
+	// 	SHA: branch,
+	// }
+
+	usePolling := flag.Bool("poll", false, "Continuously poll github repo every five minutes")
+
+	flag.Parse()
+
+	if *usePolling {
+		for {
+			pollCommits(owner, repo, branch, lastCommitSHA, client)
+
+			time.Sleep(5 * time.Minute)
+		}
+	} else {
+		pollCommits(owner, repo, branch, lastCommitSHA, client)
+	}
+}
