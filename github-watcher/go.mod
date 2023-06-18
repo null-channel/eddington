65faaca39701c95fd93f1ac1d7551b00cc762ed7
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-06-18 18:50:03 -0400

    nats and semi-complete grpc
    Co-Authored-By: s1ntaxe770r <53065463+s1ntaxe770r@users.noreply.github.com>

--- github-watcher/go.mod
+++ github-watcher/go.mod
@@ -5,4 +5,10 @@ go 1.20
 require (
 	github.com/google/go-github v17.0.0+incompatible // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/nats-io/nats.go v1.27.0 // indirect
+	github.com/nats-io/nkeys v0.4.4 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 )
--- github-watcher/main.go
+++ github-watcher/main.go
@@ -2,26 +2,53 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
-	"time"
-	"net"
 	"log"
+	"net"
 
 	"github.com/google/go-github/github"
-	"google.golang.org/grpc"
+	"github.com/nats-io/nats.go"
 	pb "github.com/null-channel/eddington/proto/github-watcher"
+	"google.golang.org/grpc"
 )
 
-var (
-	port = flag.Int("port", 10001, "github watcher server port")
-)
+// var (
+// 	// port = flag.Int("port", 10001, "github watcher server port")
 
-type server struct {
+// )
+
+// request -> Watch(xyz/project:main)
+// User [watchedRepos(xyz/project:main)]
+// for rep in watched repos GetUpdates?
+
+type WatchRepoServer struct {
 	pb.UnimplementedWatchRepoServiceServer
+	nc *nats.Conn
+	// todo: github client
+	gh *github.Client
 }
 
-func pollCommits(owner string, repo string, branch string, lastCommitSHA string, client *github.Client) {
+func (s *WatchRepoServer) WatchRepo(ctx context.Context, req *pb.WatchRepoRequest) (*pb.WatchRepoResponse, error) {
+
+	// nc, ncErr := nats.Connect("localhost:4222")
+	defer s.nc.Drain()
+
+	// if ncErr != nil {
+	// 	log.Fatal(ncErr)
+	// }
+	defer s.nc.Close()
+
+	res := s.nc.Status()
+	log.Printf("Status: %+v", res)
+
+	pollCommits(req.Owner, req.Repository, req.Branch, req.SHA, s.gh, s.nc)
+
+	return &pb.WatchRepoResponse{IsUpdated: true}
+}
+
+//
+
+func pollCommits(owner string, repo string, branch string, lastCommitSHA string, client *github.Client, nc *nats.Conn) {
 
 	ctx := context.Background()
 
@@ -40,42 +67,65 @@ func pollCommits(owner string, repo string, branch string, lastCommitSHA string,
 		fmt.Println("No updates to this branch")
 	} else {
 		fmt.Println("This branch has been updated")
-	}
 
+		if err := nc.Publish("container-builder", []byte("This branch has been updated")); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func main() {
-	client := github.NewClient(nil)
+	// client := github.NewClient(nil)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	// nc, ncErr := nats.Connect("localhost:4222")
+	// defer nc.Drain()
+
+	// if ncErr != nil {
+	// 	log.Fatal(ncErr)
+	// }
+	// defer nc.Close()
+
+	// res := nc.Status()
+	// log.Printf("Status: %+v", res)
+
+	// // owner := "null-channel"
+	// // repo := "eddington"
+	// // branch := "main"
+	// // lastCommitSHA := "02514afee6fc9ada5225a4f9670ebc4e627d6e24"
+
+	// usePolling := flag.Bool("poll", false, "Continuously poll github repo every five minutes")
+
+	// flag.Parse()
+
+	// if *usePolling {
+	// 	for {
+	// 		pollCommits(owner, repo, branch, lastCommitSHA, client, nc)
+
+	// 		time.Sleep(5 * time.Minute)
+	// 	}
+	// } else {
+	// 	pollCommits(owner, repo, branch, lastCommitSHA, client, nc)
+	// }
+
+	// lis
+	lis, err := net.Listen("tcp", "10001")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Panicf("could not open shop %s", err.Error())
 	}
 
-	grpcServer := grpc.NewServer()
+	// rpc server
+	server := grpc.NewServer()
+	nc, _ := nats.Connect(nats.DefaultURL)
+	gh := github.NewClient(nil)
 
-	pb.RegisterWatchRepoServiceServer(grpcServer, &server{})
-	log.Printf("server listening at %v", lis.Addr())
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	pb.RegisterWatchRepoServiceServer(server, &WatchRepoServer{
+		nc: nc,
+		gh: gh,
+	})
+
+	err = server.Serve(lis)
+	if err != nil {
+		log.Panicf("oops couldn't open up shop %s", err.Error())
 	}
 
-	owner := "null-channel"
-	repo := "eddington"
-	branch := "main"
-	lastCommitSHA := "02514afee6fc9ada5225a4f9670ebc4e627d6e24"
-
-	usePolling := flag.Bool("poll", false, "Continuously poll github repo every five minutes")
-
-	flag.Parse()
-
-	if *usePolling {
-		for {
-			pollCommits(owner, repo, branch, lastCommitSHA, client)
-
-			time.Sleep(5 * time.Minute)
-		}
-	} else {
-		pollCommits(owner, repo, branch, lastCommitSHA, client)
-	}
 }
