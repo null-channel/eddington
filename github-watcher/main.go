c2eeaa0dbdc00a134a24c917ee7e8a45112100d5
David Garcia <87497958+Dgarc359@users.noreply.github.com>
2023-06-18 18:57:39 -0400

    remove commented code

--- github-watcher/main.go
+++ github-watcher/main.go
@@ -12,11 +12,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-// var (
-// 	// port = flag.Int("port", 10001, "github watcher server port")
-
-// )
-
 // request -> Watch(xyz/project:main)
 // User [watchedRepos(xyz/project:main)]
 // for rep in watched repos GetUpdates?
@@ -30,12 +25,8 @@ type WatchRepoServer struct {
 
 func (s *WatchRepoServer) WatchRepo(ctx context.Context, req *pb.WatchRepoRequest) (*pb.WatchRepoResponse, error) {
 
-	// nc, ncErr := nats.Connect("localhost:4222")
 	defer s.nc.Drain()
 
-	// if ncErr != nil {
-	// 	log.Fatal(ncErr)
-	// }
 	defer s.nc.Close()
 
 	res := s.nc.Status()
@@ -43,10 +34,8 @@ func (s *WatchRepoServer) WatchRepo(ctx context.Context, req *pb.WatchRepoReques
 
 	pollCommits(req.Owner, req.Repository, req.Branch, req.SHA, s.gh, s.nc)
 
-	return &pb.WatchRepoResponse{IsUpdated: true}
+	return &pb.WatchRepoResponse{IsUpdated: true}, nil
 }
-
-//
 
 func pollCommits(owner string, repo string, branch string, lastCommitSHA string, client *github.Client, nc *nats.Conn) {
 
@@ -75,39 +64,6 @@ func pollCommits(owner string, repo string, branch string, lastCommitSHA string,
 }
 
 func main() {
-	// client := github.NewClient(nil)
-
-	// nc, ncErr := nats.Connect("localhost:4222")
-	// defer nc.Drain()
-
-	// if ncErr != nil {
-	// 	log.Fatal(ncErr)
-	// }
-	// defer nc.Close()
-
-	// res := nc.Status()
-	// log.Printf("Status: %+v", res)
-
-	// // owner := "null-channel"
-	// // repo := "eddington"
-	// // branch := "main"
-	// // lastCommitSHA := "02514afee6fc9ada5225a4f9670ebc4e627d6e24"
-
-	// usePolling := flag.Bool("poll", false, "Continuously poll github repo every five minutes")
-
-	// flag.Parse()
-
-	// if *usePolling {
-	// 	for {
-	// 		pollCommits(owner, repo, branch, lastCommitSHA, client, nc)
-
-	// 		time.Sleep(5 * time.Minute)
-	// 	}
-	// } else {
-	// 	pollCommits(owner, repo, branch, lastCommitSHA, client, nc)
-	// }
-
-	// lis
 	lis, err := net.Listen("tcp", "10001")
 	if err != nil {
 		log.Panicf("could not open shop %s", err.Error())
@@ -127,5 +83,4 @@ func main() {
 	if err != nil {
 		log.Panicf("oops couldn't open up shop %s", err.Error())
 	}
-
 }
