5064c887af3e3c9024aefe34672fa0330b242b23
s1ntaxe770r <cyberdev01@protonmail.com>
2023-07-05 23:40:50 +0100

    rm dead code

--- application/container-builder/main.go
+++ application/container-builder/main.go
@@ -7,39 +7,8 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// go-git is a highly extensible git implementation in pure Go.
-// https://github.com/go-git/go-git
-
-// Basic example of how to clone a repository using clone options.
 func main() {
-	// url := "https://github.com/null-channel/eddington.git"
-	// directory := "./tmp/eddington"
 
-	// r, err := git.PlainClone(directory, false, &git.CloneOptions{
-	// 	URL:               url,
-	// 	RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
-	// })
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// // ... retrieving the branch being pointed by HEAD
-	// ref, err := r.Head()
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(ref)
-
-	// // ... retrieving the commit object
-	// commit, err := r.CommitObject(ref.Hash())
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(commit)
 	ctx := context.Background()
 	b, err := dockerfile.NewBuilder(ctx)
 	if err != nil {
