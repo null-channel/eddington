f166450e0233694c2e93750560e9b091051805b7
atnomoverflow <montaser-abdmajid.zehri@enis.tn>
2023-07-08 11:57:52 +0100

    clean up

--- ui/src/core/constants/navbar.const.ts
+++ ui/src/core/constants/navbar.const.ts
@@ -2,11 +2,13 @@ import { MAIN_ROUTES, ROUTES } from ".";
 
 export const NAVBAR_BEFORE_LOGIN = [
   { ...ROUTES.HOME },
-  { ...ROUTES.CONTACT_US },
   { ...ROUTES.LOGIN, hiddenOnDesktop: true },
   { ...ROUTES.SIGNUP, hiddenOnDesktop: true },
 ];
 export const NAVBAR_AFTER_LOGIN = [
-  { ...MAIN_ROUTES.CONTAINER },
-  { ...ROUTES.CONTACT_US },
+  { ...ROUTES.HOME },
+  {
+    ...MAIN_ROUTES.CONTAINER,
+    path: `${ROUTES.MAIN.path}/${MAIN_ROUTES.CONTAINER.path}`,
+  },
 ];
--- ui/src/core/constants/routes.const.ts
+++ ui/src/core/constants/routes.const.ts
@@ -4,7 +4,6 @@ const ROUTES = {
   HOME: { name: "home", path: "/" },
   LOGIN: { name: "login", path: "/login" },
   SIGNUP: { name: "sign-up", path: "/sign-up" },
-  CONTACT_US: { name: "contact-us", path: "/contact-us" },
   RESET_PASSWORD: { name: "reset-password", path: "/resetpassword" },
   UPDATE_PASSWORD: { name: "update-password", path: "/updatepassword" },
 };
