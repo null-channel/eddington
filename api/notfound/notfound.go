675012fb618cf427bccd0eb8a4011ac5b0e3ba17
Marek Counts <countsmarek@gmail.com>
2023-07-15 11:43:48 -0400

    more not found data

--- api/notfound/notfound.go
+++ api/notfound/notfound.go
@@ -9,8 +9,12 @@ import (
 func NotFoundHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{
-			"error": "Not Found",
-			"path":  c.Request.URL.Path,
+			"error":   "Not Found",
+			"path":    c.Request.URL.Path,
+			"verb":    c.Request.Method,
+			"rawPath": c.Request.URL.RawPath,
+			"headers": c.Request.Header,
+			"URI":     c.Request.RequestURI,
 		})
 	}
 }
