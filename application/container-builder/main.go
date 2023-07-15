d539a132ed249220cbb68ee260374ea341376e08
s1ntaxe770r <cyberdev01@protonmail.com>
2023-07-15 18:37:34 +0100

    nuke old stuff

--- application/container-builder/main.go
+++ application/container-builder/main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"context"
-
-	"github.com/null-channel/eddington/application/container-builder/internal/containers/dockerfile"
-	"github.com/sirupsen/logrus"
-)
-
-var (
-	builkitdaddr = "tcp://0.0.0.0:4000"
-)
-
 func main() {
-
-	ctx := context.Background()
-	b, err := dockerfile.NewBuilder(ctx, builkitdaddr)
-	if err != nil {
-		logrus.Panic("unable to create builder error: ", err.Error())
-	}
-	opts := dockerfile.BuildOpt{
-		Dockerfile: "./Dockerfile",
-		ImageName:  "eddington",
-	}
-	err = b.Build(opts)
-	if err != nil {
-		logrus.Panic("unable to build image error: ", err.Error())
-	}
 
 }
