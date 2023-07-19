111251afeb1e227a4c08e97cc3dd1bddcaab7203
s1ntaxe770r <cyberdev01@protonmail.com>
2023-07-19 01:14:20 +0100

    init models package

---
+++ application/container-builder/models/build.go
@@ -0,0 +1,10 @@
+package models
+
+import "github.com/null-channel/eddington/proto/container"
+
+type Build struct {
+	CustomerID string                    `bun:"customer_id,notnull"`
+	RepoName   string                    `bun:"name,notnull"`
+	BuildID    string                    `bun:"build_id,pk"`
+	Status     container.ContainerStatus `bun:"status,default:0"`
+}
