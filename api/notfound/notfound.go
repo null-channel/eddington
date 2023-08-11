5c1a4350f57b85c300ec4a7331aba9dfc91c5d33
Marek Counts <countsmarek@gmail.com>
2023-08-11 15:15:02 -0400

    updated not found middleware

--- api/notfound/notfound.go
+++ api/notfound/notfound.go
@@ -2,30 +2,38 @@ package notfound
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("not found!!!")
 	w.WriteHeader(http.StatusNotFound)
 	w.Header().Set("Content-Type", "application/json")
-	rb, _ := json.Marshal(NotFound{
-		err:     "Not Found",
-		path:    r.URL.Path,
-		verb:    r.Method,
-		rawPath: r.URL.RawPath,
-		headers: r.Header,
-		uri:     r.RequestURI,
+	rb, err := json.Marshal(NotFound{
+		Err:     "Not Found",
+		Path:    r.URL.Path,
+		Verb:    r.Method,
+		RawPath: r.URL.RawPath,
+		Headers: r.Header,
+		Uri:     r.RequestURI,
 	})
+
+	if err != nil {
+		fmt.Println("error with json!")
+		fmt.Println(err)
+	}
 
 	w.Write(rb)
 
 }
 
 type NotFound struct {
-	err     string `json:"error"`
-	path    string
-	verb    string
-	rawPath string
-	headers map[string][]string
-	uri     string
+	Err     string              `json:"error"`
+	Path    string              `json:"path"`
+	Verb    string              `json:"verb"`
+	RawPath string              `json:"rawPath"`
+	Headers map[string][]string `json:"headers"`
+	Uri     string              `json:"uri"`
 }
