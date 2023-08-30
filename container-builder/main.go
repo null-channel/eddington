153aea57e61510456db65b57edfd683e2121bbc7
s1ntaxe770r <cyberdev01@protonmail.com>
2023-08-30 05:02:28 +0100

    dependency injection?

--- container-builder/main.go
+++ container-builder/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"net"
+	"os"
 
+	pack "github.com/buildpacks/pack/pkg/client"
+	image "github.com/null-channel/eddington/container-builder/internal/containers/buildpack"
 	"github.com/null-channel/eddington/container-builder/server"
+	"github.com/null-channel/eddington/container-builder/utils"
 	"github.com/null-channel/eddington/proto/container"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -18,7 +24,30 @@ func main() {
 	}
 	grpc := grpc.NewServer()
 	reflection.Register(grpc)
-	server, err := server.NewServer()
+
+	l := zerolog.New(os.Stdout).With().Timestamp().Str("component", "server").Logger()
+
+	db, err := utils.NewDB("")
+	if err != nil {
+		l.Err(err).Str("failed to create db", "")
+	}
+
+	packClient, err := pack.NewClient()
+	if err != nil {
+		l.Error().Err(err).Msg("unable to create pack client ")
+	}
+
+	registry := os.Getenv("REGISTRY_URL")
+	if registry == "" {
+		l.Error().Err(err).Msg("unable to create registry ")
+	}
+
+	builder, err := image.NewBuilder(db, packClient, registry)
+	if err != nil {
+		l.Error().Err(err).Msg("unable to create builder")
+	}
+
+	server, err := server.NewServer(db, builder, &l)
 	if err != nil {
 		log.Fatal().Err(err).Msg("unable to create server")
 	}
